d9ab906fee218b70a0e6f91d0b472a18dc5ae757
Chandler Morrison <cwm795@gmail.com>
2017-07-20 14:13:32 -0500

    add jquery to clear input field

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,4 +5,9 @@ let $ = require('jquery');
 let moviesController = require('./movies-controller');
 let moviesFactory = require('./movies-factory');
 
+
+function clearInput() {
+
+}
+
 // moviesController.function();
