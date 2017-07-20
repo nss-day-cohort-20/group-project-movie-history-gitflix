6d97f95515f83d2541c8e9cc25d69d87aecb54cc
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-20 09:25:21 -0500

    pull down remote master and merge into local master

---
+++ javascripts/movies-controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let $ = require('jquery');
+
+let input = document.getElementById("input");
+let findnewMoviebtn = document.getElementById("findnewMoviebtn");
+let watchedMoviebtn = document.getElementByid("watchedMoviebtn");
+let addtoWatchList = document.getElementById("addtoWatchList");
+let rateStarsbtn = document.getElementById("rateStarsbtn");
+let searchUserMoviebtn = document.getElementByid("searchUserMoviebtn");
+let deleteMoviebtn = document.getElementById("deleteMoviebtn");
+let movieContainer = document.getElementById("movieContainer");
+let showUnwatched = "";
+let showWatched = "";
+let selectedMovie = "";
+
+console.log("this is running");
+
+findnewMoviebtn.addEventListener("click");
+watchedMoviebtn.addEventListener("click");
+addtoWatchList.addEventListener("click");
+rateStarsbtn.addEventListener("click");
+input.addEventListener("click");
+searchUserMoviebtn.addEventListener("click");
+deleteMoviebtn.addEventListener("click");
+movieContainer.addEventListener("click"); 
+
