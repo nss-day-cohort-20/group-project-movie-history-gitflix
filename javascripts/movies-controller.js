8133c0da0c8c4ad2a796dc05bb9469bfcbc265b6
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-20 13:28:15 -0500

    fix bugs in controller and get the find new movie function to work correctly

--- javascripts/movies-controller.js
+++ javascripts/movies-controller.js
@@ -1,32 +1,23 @@
 'use strict';
 
-let $ = require('jquery');
-let db = require('./movies-factory');
-
+let $ = require("jquery");
+let db = require("./movies-factory");
 console.log($("#login"));
-
-let input = $("userInput").click(function (){});
-let findNewMovie = $("findNewMovieBtn").click(function (){});
-let searchUserMovies = $("searchUserMovieBtn").click(function (){});
-let watchedMovie = $("watchedMovieBtn").click(function (){});
-let addToWatchList = $("addToWatchList").click(function (){});
-let ratingStarsBtn = $("ratingStarsBtn").click(function (){});
-let deleteMovie = $("deleteMoviebtn").click(function (){});
-let movieContainer = $("movieContainer").click(function (){});
-let showUnwatched = "";
-let showWatched = "";
-let selectedMovie = "";
-
-console.log("this is also running");
 
 // defines functions findNewMoviesBtn- when the user enters the name of a movie in the input field and clicks on the find new movie button, the database is pulled up and all movies related to what the user entered in the input are displayed in the moviesContainer
 
-module.exports.findNewMoviesBtn = () => {
-	return new Promise( (resolve, reject) => {
-		$("#").click(function() {
-	
-		});
-	});
-};
+$("#findNewMovieBtn").click(function() {
+	db.findNewMovie($('#input').val());
+});
+
+$("#searchUserMovieBtn").click(function (){});
+$("#watchedMovieBtn").click(function (){});
+$("#addToWatchList").click(function (){});
+$("#ratingStarsBtn").click(function (){});
+$("#deleteMoviebtn").click(function (){});
+$(".movieContainer").click(function (){});
+let showUnwatched = "";
+let showWatched = "";
+let selectedMovie = "";
 
 
--- javascripts/movies-factory.js
+++ javascripts/movies-factory.js
@@ -4,6 +4,9 @@ let $ = require('jquery');
 // let fbURL = "https://fir-101-6198a.firebaseio.com";
 // let firebase = require('./firebaseConfig');
 
-
+// 
+module.exports.findNewMovie = (searchString) => {
+	console.log(searchString);
+};
 
 
