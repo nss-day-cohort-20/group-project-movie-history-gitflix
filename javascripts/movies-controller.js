4c5dc0f1d1a02b2a9ee215323c6bba26bf7171d9
Chandler Morrison <cwm795@gmail.com>
2017-07-21 13:56:25 -0500

    fixing bugs

--- javascripts/movies-controller.js
+++ javascripts/movies-controller.js
@@ -2,12 +2,15 @@
 
 let $ = require("jquery");
 let db = require("./movies-factory");
-let dom = require("./main.js");
+
+let dom = require("./main");
+
 let tb = require("./template-builder");
 console.log($("#login"));
 let movieArray;
 
 // defines functions findNewMoviesBtn- when the user enters the name of a movie in the input field and clicks on the find new movie button, the database is pulled up and all movies related to what the user entered in the input are displayed in the moviesContainer
+
 
 $("#findNewMovieBtn").click(function(){
 	db.findNewMovie($('#input').val())
@@ -17,8 +20,10 @@ $("#findNewMovieBtn").click(function(){
 	});
 });
 
+
 // defines function storeMovieData while expecting movies to be passed in
 function storeMovieData(movies) {
+
 	movieArray = movies;
 	let movieList = tb.displayMovieData(movies);
 	$('.container').html(movieList);
@@ -53,6 +58,32 @@ function buildMovieObjects(movies) {
 //     $('container').html(songList);
 //   });
 // };
+// =======
+//     movieArray = movies;
+//     console.log("Movies = ", movies);
+//     tb.displayMovieData(movies);
+// }
+
+// function buildMovieObj() {
+//     let movieObj = {
+//         Title: $("# form--title").val(),
+//         year: $("# form--year").val(),
+//         actors: $("# form--actors").val(),
+//         poster: $("# form--poster").val(),
+//         rating: $("# form--rating").val()
+//     };
+//     return movieObj;
+// }
+
+// module.exports.loadMoviesToDom = () => {
+//     db.getMovies()
+//         .then((movieData) => {
+//             console.log("movieData", movieData);
+//             let movieList = templates.makeSongList(movieData);
+//             $(".container").html(movieList);
+//         });
+// };
+// >>>>>>> Stashed changes
 
 // $("#searchUserMovieBtn").click(function() {
 // 	db.searchUserMovie($('#input').val());
